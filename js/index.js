d8bc23984066de824b4970a40248890c175dae17
Sileye-Franchet <sileye.franchet@etu.univ-orleans.fr>
2026-03-17 12:30:59 +0100

    debut router

--- js/index.js
+++ js/index.js
@@ -1,8 +1,38 @@
-import Home from "./views/Home.js";
-import EntityAll from "./views/EntityAll.js";
+const { createApp } = Vue;
+const { createRouter, createWebHashHistory } = VueRouter;
 
-const path = {
-    path: "",
+const Home = {
+  template: `<div><h1>Home</h1></div>`,
+};
+
+const EntityAll = {
+  template: `<div><h1>Entities</h1></div>`,
+};
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
     component: Home,
-    alias: ["/" ,"home", "accueil"]
-}
+    alias: ["/home", "/accueil"],
+  },
+  {
+    path: "/entities",
+    name: "Entities",
+    component: EntityAll,
+    alias: "/entites",
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    redirect: { name: "Home" },
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+const app = createApp({});
+app.use(router);
+app.mount("#app");
